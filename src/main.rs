aa67b8c44c5f74845a05df3fc3061d3b1edfc1f7
Facundo Martin <facundomartin98@gmail.com>
2024-01-14 21:00:35 -0800

    refactor: implement the collect method manually

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 fn iterator() {
-    let foo: Vec<_> = vec![1,2,3].iter().map(|x| x + 1).collect();
-    println!("{:?}",foo); 
+    let data = vec![1,2,3];
+    let mut foo = data.iter().map(|x| x + 1);
+    
+    let mut new_vector = vec![];
+
+    while let Some(x) = foo.next() {
+        new_vector.push(x)
+    }
+    println!("{:?}",new_vector); 
 }
 
 fn main() {
